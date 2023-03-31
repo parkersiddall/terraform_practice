408ff3f699e21c07b3f53376768a6be2277da67f
parkersiddall <parker.siddall@gmail.com>
2023-03-31 21:53:58 +0200

    Fix bugs in userdata script

--- deploy/userdata.sh
+++ deploy/userdata.sh
@@ -5,6 +5,7 @@ sudo npm install -g pm2
 git clone https://github.com/parkersiddall/terraform_practice.git
 cd terraform_practice/api/ && npm install
 pm2 start index.js
+pm2 startup index.js 
 
-cp ../deploy/default /etc/nginx/sites-available/default
+sudo cp ../deploy/nginx/default /etc/nginx/sites-available/default
 sudo systemctl restart nginx
